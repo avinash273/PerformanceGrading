5e2359561a03406f42309a7578d83347346b23c7
avinash273 <savinash273@gmail.com>
2024-08-03 00:23:04 -0500

    fetchData enabled CORS

--- src/main/java/com/neural/PerformanceGrading/repository/StudentRepository.java
+++ src/main/java/com/neural/PerformanceGrading/repository/StudentRepository.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.stream.Collectors;
+
 import java.util.List;
 
 @Repository
