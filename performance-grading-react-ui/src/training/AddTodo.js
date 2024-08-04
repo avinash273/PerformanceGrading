e19a8e5ba5272c155aa4c87569f03665cda82217
avinash273 <savinash273@gmail.com>
2024-08-04 12:38:32 -0500

    Test

---
+++ performance-grading-react-ui/src/training/AddTodo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function AddTodo() {
+
+    return (
+        < >
+        <div className="mt-5">
+            <form>
+                <div className="mb-3">
+                    <label className="form-label">Assigned</label>
+                    <input type="text" className="form-control"></input>
+                </div>
+            </form>
+        </div>
+        </>
+    )
+
+}
+
+export default AddTodo;
--- performance-grading-react-ui/src/training/TodoTable.js
+++ performance-grading-react-ui/src/training/TodoTable.js
@@ -1,23 +1,24 @@
 import TodoRowItem from './TodoRowItem';
 import React, {useState, useEffect} from "react";
 
-const todos = [{rowNumber : 1, rowDescription : 'Wake up', rowAssigned : 'user 1'},
+function TodoTable() {
+
+  const [todos, setTodos] = useState([
+    {rowNumber : 1, rowDescription : 'Wake up', rowAssigned : 'user 1'},
     {rowNumber : 2, rowDescription : 'Brush', rowAssigned : 'user 2'},
     {rowNumber : 3, rowDescription : 'Eat breakfast', rowAssigned : 'user 3'},
     {rowNumber : 4, rowDescription : 'Go to office', rowAssigned : 'user 4'}
-  ];
-
-function TodoTable() {
+  ]);
 
     const addTodo = () => {
         console.log(todos);
         if(todos.length > 0){
             const newTodo = {
                 rowNumber : todos.length + 1,
-                rowDescription : 'New Todo' + todos.length + 1,
-                rowAssigned : 'user ' + todos.length + 1
+                rowDescription : 'New Todo' + (todos.length + 1),
+                rowAssigned : 'user ' + (todos.length + 1)
             }
-            todos.push(newTodo);
+            setTodos(todos => [...todos, newTodo])
         }
 
         // console.log(todos);
