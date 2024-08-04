e88bdca48fbf9790eda466366b51757c3c24c6e9
avinash273 <savinash273@gmail.com>
2024-08-04 11:05:37 -0500

    useState on Todo

--- performance-grading-react-ui/src/training/TodoTable.js
+++ performance-grading-react-ui/src/training/TodoTable.js
@@ -1,17 +1,31 @@
 import TodoRowItem from './TodoRowItem';
+import React, {useState, useEffect} from "react";
+
+const todos = [{rowNumber : 1, rowDescription : 'Wake up', rowAssigned : 'user 1'},
+    {rowNumber : 2, rowDescription : 'Brush', rowAssigned : 'user 2'},
+    {rowNumber : 3, rowDescription : 'Eat breakfast', rowAssigned : 'user 3'},
+    {rowNumber : 4, rowDescription : 'Go to office', rowAssigned : 'user 4'}
+  ];
 
 function TodoTable() {
 
-    const todos = [{rowNumber : 1, rowDescription : 'Wake up', rowAssigned : 'user 1'},
-        {rowNumber : 2, rowDescription : 'Brush', rowAssigned : 'user 2'},
-        {rowNumber : 3, rowDescription : 'Eat breakfast', rowAssigned : 'user 3'},
-        {rowNumber : 4, rowDescription : 'Go to office', rowAssigned : 'user 4'}
-      ];
+    const addTodo = () => {
+        console.log(todos);
+        if(todos.length > 0){
+            const newTodo = {
+                rowNumber : todos.length + 1,
+                rowDescription : 'New Todo' + todos.length + 1,
+                rowAssigned : 'user ' + todos.length + 1
+            }
+            todos.push(newTodo);
+        }
 
+        console.log(todos);
+    }
     return(
         <>
         <div className='mt-5 container'>
-          <div>
+          <div className='card'>
           <h1>Your Todo's</h1>
             <table>
               <thead>
@@ -25,13 +39,15 @@ function TodoTable() {
 
                 {
                   todos.map(task => (
-                    <TodoRowItem 
+                    <TodoRowItem
                     rowNumber = {task.rowNumber}
                     rowDescription = {task.rowDescription}
                     rowAssigned = {task.rowAssigned}
                     />
                   ))
                 }
+
+                <button className='btn btn-primary' onClick={addTodo}>Add new todo</button>
 
               </tbody>
             </table>
